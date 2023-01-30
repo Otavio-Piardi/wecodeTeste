4e9b67b48147f9edb8994393f19db1f3fb037430
tavvio <otaviopiardi@gmail.com>
2023-01-30 16:52:38 -0300

    Detalhes Visuais e Revisão

--- js/script.js
+++ js/script.js
@@ -13,10 +13,10 @@ document.querySelector('#btn-fechar-menu-lateral').addEventListener('click', e =
 })
 
 function menuSwitch() {
-    if(abrirMenu){
+    if (abrirMenu) {
         document.querySelector('#menu-lateral').style.marginLeft = 0
         return
-    } 
+    }
     document.querySelector('#menu-lateral').style.marginLeft = `-${menuTamanho}`
 }
 
@@ -33,10 +33,10 @@ document.querySelector('#btn-fechar-menu-pesquisa').addEventListener('click', e 
 })
 
 function menuPesquisaSwitch() {
-    if(abrirMenuPesquisa){
+    if (abrirMenuPesquisa) {
         document.querySelector('#menu-pesquisa').style.display = 'block'
         return
-    } 
+    }
     document.querySelector('#menu-pesquisa').style.display = 'none'
 }
 
@@ -47,8 +47,7 @@ const linkGavetaDepartamento5 = document.getElementById('link-gaveta-departament
 const linkGavetaDepartamento6 = document.getElementById('link-gaveta-departamento-6')
 const linkGavetaDepartamento7 = document.getElementById('link-gaveta-departamento-7')
 
-//uma função para cada?? :/
-function toggleGavetaDepartamento2(){
+function toggleGavetaDepartamento2() {
     const ulGavetaDepartamento2 = document.getElementById('elementos-gaveta-departamento-2')
     const liParteMenuDepartamento2 = document.getElementById('departamento-2')
     ulGavetaDepartamento2.classList.toggle('ul-ativo')
@@ -56,14 +55,14 @@ function toggleGavetaDepartamento2(){
     linkGavetaDepartamento2.classList.toggle('link-ativo')
 }
 
-function toggleGavetaDepartamento3(){
+function toggleGavetaDepartamento3() {
     const ulGavetaDepartamento3 = document.getElementById('elementos-gaveta-departamento-3')
     const liParteMenuDepartamento3 = document.getElementById('departamento-3')
     ulGavetaDepartamento3.classList.toggle('ul-ativo')
     liParteMenuDepartamento3.classList.toggle('li-ativo')
     linkGavetaDepartamento3.classList.toggle('link-ativo')
 }
-function toggleGavetaDepartamento4(){
+function toggleGavetaDepartamento4() {
     const ulGavetaDepartamento4 = document.getElementById('elementos-gaveta-departamento-4')
     const liParteMenuDepartamento4 = document.getElementById('departamento-4')
     ulGavetaDepartamento4.classList.toggle('ul-ativo')
@@ -71,7 +70,7 @@ function toggleGavetaDepartamento4(){
     linkGavetaDepartamento4.classList.toggle('link-ativo')
 }
 
-function toggleGavetaDepartamento5(){
+function toggleGavetaDepartamento5() {
     const ulGavetaDepartamento5 = document.getElementById('elementos-gaveta-departamento-5')
     const liParteMenuDepartamento5 = document.getElementById('departamento-5')
     ulGavetaDepartamento5.classList.toggle('ul-ativo')
@@ -79,7 +78,7 @@ function toggleGavetaDepartamento5(){
     linkGavetaDepartamento5.classList.toggle('link-ativo')
 }
 
-function toggleGavetaDepartamento6(){
+function toggleGavetaDepartamento6() {
     const ulGavetaDepartamento6 = document.getElementById('elementos-gaveta-departamento-6')
     const liParteMenuDepartamento6 = document.getElementById('departamento-6')
     ulGavetaDepartamento6.classList.toggle('ul-ativo')
@@ -87,7 +86,7 @@ function toggleGavetaDepartamento6(){
     linkGavetaDepartamento6.classList.toggle('link-ativo')
 }
 
-function toggleGavetaDepartamento7(){
+function toggleGavetaDepartamento7() {
     const ulGavetaDepartamento7 = document.getElementById('elementos-gaveta-departamento-7')
     const liParteMenuDepartamento7 = document.getElementById('departamento-7')
     ulGavetaDepartamento7.classList.toggle('ul-ativo')
@@ -108,46 +107,46 @@ const linkGavetaConta = document.getElementById('link-gaveta-conta')
 const linkGavetaPoliticas = document.getElementById('link-gaveta-politicas')
 const linkGavetaContato = document.getElementById('link-gaveta-contato')
 
-function toggleGavetaSobre(){
+function toggleGavetaSobre() {
     const ulGavetaSobre = document.getElementById('elementos-gaveta-sobre')
     const pGavetaSobre = document.getElementById('p-mais-menos-sobre')
     pGavetaSobre.innerHTML = "-"
     ulGavetaSobre.classList.toggle('ul-ativo')
     linkGavetaSobre.classList.toggle('link-ativo-footer')
-    if((pGavetaSobre.innerHTML = "+") && ulGavetaSobre.classList.contains('ul-ativo')) {
+    if ((pGavetaSobre.innerHTML = "+") && ulGavetaSobre.classList.contains('ul-ativo')) {
         pGavetaSobre.innerHTML = "-"
     }
 }
 
-function toggleGavetaConta(){
+function toggleGavetaConta() {
     const ulGavetaConta = document.getElementById('elementos-gaveta-conta')
     const pGavetaConta = document.getElementById('p-mais-menos-conta')
     pGavetaConta.innerHTML = "-"
     ulGavetaConta.classList.toggle('ul-ativo')
     linkGavetaConta.classList.toggle('link-ativo-footer')
-    if((pGavetaConta.innerHTML = "+") && ulGavetaConta.classList.contains('ul-ativo')) {
+    if ((pGavetaConta.innerHTML = "+") && ulGavetaConta.classList.contains('ul-ativo')) {
         pGavetaConta.innerHTML = "-"
     }
 }
 
-function toggleGavetaPoliticas(){
+function toggleGavetaPoliticas() {
     const ulGavetaPoliticas = document.getElementById('elementos-gaveta-politicas')
     const pGavetaPoliticas = document.getElementById('p-mais-menos-politicas')
     pGavetaPoliticas.innerHTML = "-"
     ulGavetaPoliticas.classList.toggle('ul-ativo')
     linkGavetaPoliticas.classList.toggle('link-ativo-footer')
-    if((pGavetaPoliticas.innerHTML = "+") && ulGavetaPoliticas.classList.contains('ul-ativo')) {
+    if ((pGavetaPoliticas.innerHTML = "+") && ulGavetaPoliticas.classList.contains('ul-ativo')) {
         pGavetaPoliticas.innerHTML = "-"
     }
 }
 
-function toggleGavetaContato(){
+function toggleGavetaContato() {
     const ulGavetaContato = document.getElementById('elementos-gaveta-contato')
     const pGavetaContato = document.getElementById('p-mais-menos-contato')
     pGavetaContato.innerHTML = "-"
     ulGavetaContato.classList.toggle('ul-ativo')
     linkGavetaContato.classList.toggle('link-ativo-footer')
-    if((pGavetaContato.innerHTML = "+") && ulGavetaContato.classList.contains('ul-ativo')) {
+    if ((pGavetaContato.innerHTML = "+") && ulGavetaContato.classList.contains('ul-ativo')) {
         pGavetaContato.innerHTML = "-"
     }
 }
